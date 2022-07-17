d88fa9b9adabc6413e4deec644642b36d371ea90
Jeff Smykil <jsmykil@eb.com>
2022-07-17 12:59:14 -0500

    feature error/not found page and image

--- src/Pages/NotFound/NotFound.tsx
+++ src/Pages/NotFound/NotFound.tsx
@@ -1,7 +1,43 @@
-export const NotFound = () => {
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { Link } from "react-router-dom";
+import { Container } from "@mui/system";
+
+export function NotFound() {
   return (
-    <>
-      <h1>Not found</h1>
-    </>
+    <Container maxWidth="xs" sx={{ marginTop: "3em" }}>
+      <Card sx={{ minWidth: 275 }}>
+        <CardContent>
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+            Oh, oh.
+          </Typography>
+          <Typography mb="1em" variant="h5" component="div">
+            You seem to have lost the pheremone trail...
+          </Typography>
+          <img
+            width="320"
+            height="240"
+            src="/images/ant_columbo.png"
+            alt="A confused ant."
+          />
+          <Typography mt="1em" textAlign="center" component="p">
+            Courtesy{" "}
+            <a href="https://sciantsmedia.com" target="blank">
+              Sciants Media
+            </a>{" "}
+            &#169;
+            {new Date().getFullYear()}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Head back to the colonoy</Button>
+        </CardActions>
+      </Card>
+    </Container>
   );
-};
+}
