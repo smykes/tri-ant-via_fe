b16a4e36285c914e7366d0659ba8437337b30d1b
smykes <jeff.smykil@gmail.com>
2025-10-28 14:48:33 -0500

    global fetch

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,9 +1,9 @@
-import "@testing-library/jest-dom";
-import { fetch } from "cross-fetch";
-import { server } from "./mocks/server";
-import { afterAll, afterEach, beforeAll } from "vitest";
-global.fetch = fetch;
+import '@testing-library/jest-dom';
+import { fetch } from 'cross-fetch';
+import { server } from './mocks/server';
+import { afterAll, afterEach, beforeAll } from 'vitest';
+(globalThis as any).fetch = fetch;
 
-beforeAll(() => server.listen({ onUnhandledRequest: "warn" }));
+beforeAll(() => server.listen({ onUnhandledRequest: 'warn' }));
 afterAll(() => server.close());
 afterEach(() => server.resetHandlers());
